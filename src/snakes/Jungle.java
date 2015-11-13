9c7fabbee8a835c545c7858f7814277e569ef36f
WyattCampbell52 <WyattCampbell@Wyatts-MacBook-Air.local>
2015-11-13 09:04:43 -0800

    Released Key

--- src/snakes/Jungle.java
+++ src/snakes/Jungle.java
@@ -20,11 +20,14 @@ import java.awt.event.MouseEvent;
 class Jungle extends Environment {
 
     Grid grid;
-    
+    Point point;
+    Point snake;
+    int x;
+    int y;
     
     public Jungle() {
         
-        grid = new Grid(1000, 1000, 10, 10, new Point(0,50), Color.black);
+        grid = new Grid(32, 20, 25, 25, new Point(50,50), Color.black);
         
     }
 
@@ -33,28 +36,52 @@ class Jungle extends Environment {
         
     }
 
+    int counter;
+    
     @Override
     public void timerTaskHandler() {
-        
+//        System.out.println("#" + ++counter);
     }
 
     @Override
     public void keyPressedHandler(KeyEvent e) {
-        
+//        System.out.println("Key Event " + e.getKeyChar());
+//        System.out.println("Key Event " + e.getKeyCode());
+      
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            x = x + 1;
+            System.out.println("Right");
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            x = x - 1;
+            System.out.println("Left");
+        } else if (e.getKeyCode() == KeyEvent.VK_UP) {
+            y = x - 1;
+            System.out.println("Up");
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            y = x + 1;
+            System.out.println("Down");
+        }  
+        System.out.println(x);
+        System.out.println(y);
     }
 
     @Override
     public void keyReleasedHandler(KeyEvent e) {
-        
+        if (e.getKeyCode() == KeyEvent.VK_W) {
+            System.out.println("Released Up");
+        }else if (e.getKeyCode() == KeyEvent.VK_A) {
+            System.out.println("Released Left");
+        }else if (e.getKeyCode() == KeyEvent.VK_S) {
+            System.out.println("Released Down");
+        }else if (e.getKeyCode() == KeyEvent.VK_D) {
+            System.out.println("Released RIght");
+        }
     }
 
     @Override
     public void environmentMouseClicked(MouseEvent e) {
-        System.out.println("mouse clicked at " + e.getPoint());
-        System.out.println("In cell " + grid.getCellLocationFromSystemCoordinate(e.getPoint()));
-        
-        
-        
+        System.out.println("mouse clicked at " + e.getPoint());        
+        System.out.println("In cell " + grid.getCellLocationFromSystemCoordinate( point = e.getPoint()));
     }
 
     @Override
@@ -62,6 +89,7 @@ class Jungle extends Environment {
         if (grid != null) {
             grid.paintComponent(graphics);
         }
+        
     }
     
 }
